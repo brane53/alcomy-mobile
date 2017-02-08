bfc30837d5592e21361a962dedb18c9418f58c72
Brane Vrajich <brane53@gmail.com>
2017-02-07 20:21:35 -0700

    delete font files

--- src/pages/residents-tabs/residents-tabs.ts
+++ src/pages/residents-tabs/residents-tabs.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /*
-  Generated class for the ResidentTabs page.
+  Generated class for the ResidentsTabs page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-resident-tabs',
-  templateUrl: 'resident-tabs.html'
+  selector: 'page-residents-tabs',
+  templateUrl: 'residents-tabs.html'
 })
-export class ResidentTabsPage {
+export class ResidentsTabsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ResidentTabsPage');
+    console.log('ionViewDidLoad ResidentsTabsPage');
   }
 
 }
